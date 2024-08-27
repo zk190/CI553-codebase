000d148d3fa4243bf441a10a6ef2ef43c1c58a88
ShineShan2023 <135724559+ShineShan2023@users.noreply.github.com>
2024-08-27 19:14:32 +0100

    Add files via upload

---
+++ catalogue/BetterBasket.java
@@ -0,0 +1,20 @@
+package catalogue;
+
+import java.io.Serializable;
+import java.util.Collections;
+
+/**
+ * Write a description of class BetterBasket here.
+ * 
+ * @author  Your Name 
+ * @version 1.0
+ */
+public class BetterBasket extends Basket implements Serializable
+{
+  private static final long serialVersionUID = 1L;
+  
+
+  // You need to add code here
+  // merge the items for same product,
+  // or sort the item based on the product number
+}
