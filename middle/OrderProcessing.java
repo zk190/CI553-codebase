8c2c9726ac7634eef79d5398a94550ff8bf2cfe7
ShineShan2023 <135724559+ShineShan2023@users.noreply.github.com>
2024-08-27 19:43:38 +0100

    Add files via upload

---
+++ middle/OrderProcessing.java
@@ -0,0 +1,36 @@
+package middle;
+
+import catalogue.Basket;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+  * Defines the interface for accessing the order processing system.
+  * @author  Mike Smith University of Brighton
+  * @version 2.0
+  */
+
+public interface OrderProcessing
+{
+                                                   // Used by
+  public void newOrder(Basket bought)              // Cashier
+         throws OrderException;
+
+  public int  uniqueNumber()                       // Cashier
+         throws OrderException;
+   
+  public Basket getOrderToPack()                   // Packer
+         throws OrderException;
+ 
+  public boolean informOrderPacked(int orderNum)   // Packer 
+         throws OrderException;
+         
+  // not being used in this version
+  public boolean informOrderCollected(int orderNum) // Collection
+         throws OrderException;
+   
+  // not being used in this version
+  public Map<String,List<Integer>> getOrderState() // Display
+         throws OrderException;
+}
