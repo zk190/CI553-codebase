2d524438b89fa97f1fe3157b627a129d93a78ef4
ShineShan2023 <135724559+ShineShan2023@users.noreply.github.com>
2024-08-27 19:11:43 +0100

    Add files via upload

---
+++ clients/customer/CustomerController.java
@@ -0,0 +1,42 @@
+package clients.customer;
+
+/**
+ * The Customer Controller
+ */
+
+public class CustomerController
+{
+  private CustomerModel model = null;
+  private CustomerView  view  = null;
+
+  /**
+   * Constructor
+   * @param model The model 
+   * @param view  The view from which the interaction came
+   */
+  public CustomerController( CustomerModel model, CustomerView view )
+  {
+    this.view  = view;
+    this.model = model;
+  }
+
+  /**
+   * Check interaction from view
+   * @param pn The product number to be checked
+   */
+  public void doCheck( String pn )
+  {
+    model.doCheck(pn);
+  }
+
+  /**
+   * Clear interaction from view
+   */
+  public void doClear()
+  {
+    model.doClear();
+  }
+
+  
+}
+
